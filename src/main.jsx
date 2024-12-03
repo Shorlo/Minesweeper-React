99201c9e74e9cf24a1c2638ebfb6a599ce691fb7
Javier Sainz de Baranda y Goñi <shorlo.sbg@gmail.com>
2024-12-04 00:42:16 +0100

    added App start point

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import App from "./App.jsx";
 
-console.log("Iniciando aplicación...");
-
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>¡Hola, mundo!</h1>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
