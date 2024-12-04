ae5a11e42ea62f895fe0690aa48329051b64a2b0
Javier Sainz de Baranda y Goñi <shorlo.sbg@gmail.com>
2024-12-04 20:32:38 +0100

    added initialize method to Minesweeper class

--- src/Minesweeper/logic/Minesweeper.js
+++ src/Minesweeper/logic/Minesweeper.js
@@ -9,4 +9,22 @@ class Minesweeper{
         this.state = "initialized";
         this.matrix = [];
     }
+
+    initialize(size, mineCount){
+        if (size.x < 4 || size.y < 4) throw new Error("Board sieze too small.");
+        if (mineCount < 2 || mineCount > size.x * size.y - 9) throw new Error("Invalid number of mines");
+
+        this.size = size;
+        this.mineCount = mineCount;
+        this.remainingCount = size.x * size.y - mineCount;
+        this.flagCount = 0;
+        this.state = "pristine";
+        this.matrix = Array.from({ length: size.x * size.y }, () => ({
+            isMine: false,
+            isDisclosed: false,
+            isFlagged: false,
+            isExploded: false,
+            warning: 0
+        }));
+    }
 }
