6571add4356457f7839ff29988d3c23c4fd42ab4
Javier Sainz de Baranda y Goñi <shorlo.sbg@gmail.com>
2024-12-04 21:18:58 +0100

    added component Cell

---
+++ src/Minesweeper/Cell.jsx
@@ -0,0 +1,32 @@
+// src/Minesweeper/Cell.jsx
+
+import React from "react";
+import "./styles/Cell.css";
+
+const Cell = ({x, y, cell, onClick, onRightClick}) => {
+    const getClassNames = () => {
+        let classes = "cell";
+        if (cell.isDisclosed) classes += " disclosed";
+        if (cell.isMine && cell.isDisclosed) classes += " mine";
+        if (cell.isFlagged) classes += " flag";
+        return classes;
+    }
+
+    return (
+        <div 
+            className={getClassNames()}
+            onClick={onClick}
+            onContextMenu={onRightClick}
+        >
+            {cell.isDisclosed
+                ? cell.isMine
+                    ? "💣"
+                    : cell.warning || ""
+                : cell.isFlagged
+                ? "🚩"
+                : ""}
+        </div>
+    );
+};
+
+export default Cell;
