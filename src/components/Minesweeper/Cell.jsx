170705d43e87bffedc8408c96cdebde04e57b7a6
Javier Sainz de Baranda y Goñi <shorlo.sbg@gmail.com>
2025-01-21 23:54:15 +0100

    added JSDoc to Cell.jsx

--- src/components/Minesweeper/Cell.jsx
+++ src/components/Minesweeper/Cell.jsx
@@ -3,6 +3,18 @@
 import React from "react";
 import "./styles/Cell.css";
 
+/**
+ * Cell component
+ * 
+ * This component represents a single cell in the Minesweeper game board.
+ * It handles the display of the cell's state and manages click events.
+ * 
+ * @component
+ * @param {Object} props - The properties object.
+ * @param {Object} props.cell - The cell object containing its state.
+ * @param {Function} props.onClick - The function to handle cell click events.
+ * @param {Function} props.onRightClick - The function to handle cell right-click events.
+ */
 const Cell = ({cell, onClick, onRightClick}) => {
     const getClassNames = () => {
         let classes = "cell";
