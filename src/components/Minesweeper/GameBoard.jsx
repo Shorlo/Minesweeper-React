c73dc9d134492a7fd940213d1c8da55438a411cf
Javier Sainz de Baranda y Goñi <shorlo.sbg@gmail.com>
2025-01-21 23:53:26 +0100

    added JSDoc to GameBoard.jsx and GameContainer.jsx

--- src/components/Minesweeper/GameBoard.jsx
+++ src/components/Minesweeper/GameBoard.jsx
@@ -4,6 +4,21 @@ import React from "react";
 import Cell from "./Cell";
 import "./styles/GameBoard.css";
 
+/**
+ * GameBoard component
+ * 
+ * This component renders the Minesweeper game board.
+ * It displays the grid of cells and handles the click events on each cell.
+ * 
+ * @component
+ * @param {Object} props - The properties object.
+ * @param {Array} props.board - The game board matrix.
+ * @param {Object} props.size - The size of the game board.
+ * @param {number} props.size.x - The number of columns in the game board.
+ * @param {number} props.size.y - The number of rows in the game board.
+ * @param {Function} props.onCellClick - The function to handle cell click events.
+ * @param {Function} props.onCellRightClick - The function to handle cell right-click events.
+ */
 const GameBoard = ({ board, size, onCellClick, onCellRightClick }) => {
     return (
         <div className="game-board"
--- src/components/Minesweeper/GameContainer.jsx
+++ src/components/Minesweeper/GameContainer.jsx
@@ -4,6 +4,14 @@ import React from "react";
 import MinesweeperApp from "./MinesweeperApp";
 import "./styles/GameContainer.css"
 
+/**
+ * GameContainer component
+ * 
+ * This component serves as a container for the MinesweeperApp component.
+ * It provides a styled wrapper around the main game application.
+ * 
+ * @component
+ */
 const GameContainer = () => {
     return (
         <div className="game-container">
